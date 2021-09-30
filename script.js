3df2e07044a1e2d8bb3c81c5576517034eac0096
weslley88 <weslley.os88@gmail.com>
2021-09-30 17:34:41 -0300

    calculadora de media

---
+++ script.js
@@ -0,0 +1,23 @@
+var nomeAluno = "Weslley"
+
+var notaDoPrimeiroBimestre = 8
+var notaDoSegundoBimestre = 6
+var notaDoTerceiroBimestre = 10
+var notaDoQuartoBimestre = 4
+
+var mediaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+if (mediaFinal >= 7)
+ {
+  console.log("Aprovado")
+ }
+  else if (mediaFinal >=5)
+    {
+    console.log("Recuperação")
+    }
+  else (mediaFinal <5)
+{    
+    console.log("Reprovado")
+}
+
+var resultado = ("O aluno " nomeAluno + "teve a nota " + mediaFinal + "de média final, portanto o aluno está" + console.log(mediaFinal))
